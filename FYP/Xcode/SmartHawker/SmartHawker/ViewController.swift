b26920455d6ea9373d02d505861e8bec1783dbee
kayzongwei <zongwei.kay.2014@smu.edu.sg>
2016-06-06 23:08:32 +0800

    Creating repository for Xcode

---
+++ FYP/Xcode/SmartHawker/SmartHawker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartHawker
+//
+//  Created by Kay Zong Wei on 6/6/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
