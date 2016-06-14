788c7dd132fc0e8240f128095430d72271b91381
kayzongwei <zongwei.kay.2014@smu.edu.sg>
2016-06-14 23:37:28 +0800

    Major Updates
    Imported Parse library and linked to Parse account, completed FAQ and
    Contact Us page

--- FYP/Xcode/SmartHawker/SmartHawker/AppDelegate.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Parse.setApplicationId("p5eYUBJtyvgCZrQM5pcOGLwaorWAUJn9q95Iwwht", clientKey: "RyMdMeTL5hzX4qxDntNn4UlR2CJAXWXfWT26pjWt")
+        
         // Override point for customization after application launch.
         return true
     }
