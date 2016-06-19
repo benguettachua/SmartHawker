78f6609fe21822a8e90b86a88c9b4e5ed9a2003d
kayzongwei <zongwei.kay.2014@smu.edu.sg>
2016-06-19 20:57:39 +0800

    Half-done Registration function

---
+++ FYP/Xcode/SmartHawker/SmartHawker/RegistrationViewController.swift
@@ -0,0 +1,67 @@
+//
+//  RegistrationViewController.swift
+//  SmartHawker
+//
+//  Created by Kay Zong Wei on 19/6/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+
+
+class RegistrationViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var businessName: UITextField!
+    @IBOutlet weak var businessRegNo: UITextField!
+    @IBOutlet weak var businessAddress: UITextField!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    @IBOutlet weak var adminPIN: UITextField!
+    
+    // This function currently saves to the DB without any validation.
+    @IBAction func registerButton(sender: UIButton) {
+        let newUser = PFObject(className: "User")
+        newUser["businessName"] = businessName.text
+        newUser["businessRegNo"] = businessRegNo.text
+        newUser["businessAddress"] = businessAddress.text
+        newUser["username"] = username.text
+        newUser["email"] = email.text
+        newUser["phoneNumber"] = phoneNumber.text
+        newUser["password"] = password.text
+        newUser["adminPIN"] = adminPIN.text
+        
+        
+        newUser.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                // The object has been saved.
+            } else {
+                // There was a problem, check error.description
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Actions
+    @IBAction func selectNewImageFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .Camera
+        
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+}
