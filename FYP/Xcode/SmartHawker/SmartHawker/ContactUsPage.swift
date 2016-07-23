bf2f38c4b5dfc9f025b8527d6bcfb61d8741adb6
benguettachua <benguettachua@gmail.com>
2016-07-23 22:50:38 +0800

    added in controller view for Contact us, Helpdesk, and chinese language for enquiries pages

---
+++ FYP/Xcode/SmartHawker/SmartHawker/ContactUsPage.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  SmartHawker
+//
+//  Created by Kay Zong Wei on 20/6/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsPage: UIViewController {
+ 
+    //MARK properties
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
