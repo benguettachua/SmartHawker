2340b06b132c256c94429f4ea6e24acdd0f6dbc2
kayzongwei <zongwei.kay.2014@smu.edu.sg>
2016-08-01 22:33:27 +0800

    Validation: Creation of subuser MUST have unique PIN

--- FYP/Xcode/SmartHawker/SmartHawker/AddNewSubUserViewController.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/AddNewSubUserViewController.swift
@@ -18,23 +18,40 @@ class AddNewSubUserViewController: UIViewController {
     
     // Variables
     let user = PFUser.currentUser()
+    var PINS = [String]()
     
     // MARK: Action
     @IBAction func addSubUser(sender: UIButton) {
         let subUser = PFObject(className: "SubUser")
         subUser.ACL = PFACL(user: user!)
+        var uniquePin = false
         
+        let pinToRecord = PINTextField.text
         // Sub User Properties
         subUser["user"] = user
         subUser["name"] = nameTextField.text
-        subUser["pin"] = PINTextField.text
+        if (PINS.contains(pinToRecord!) == false) {
+            subUser["pin"] = PINTextField.text
+            uniquePin = true
+        } else {
+            uniquePin = false
+        }
         subUser["address"] = addressTextField.text
-        subUser.saveEventually()
+        if (uniquePin) {
+            subUser.saveEventually()
+        } else {
+            // do something to show save failed.
+            let alert = UIAlertController(title: "Save failed", message: "PIN entered is used for another user, PIN must be unique!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Got it!", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Get all PINS that are used
+        let defaults = NSUserDefaults.standardUserDefaults()
+        PINS = defaults.objectForKey("allPINS") as! [String]
     }
 }
