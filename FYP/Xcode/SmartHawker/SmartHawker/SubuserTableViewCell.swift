b2e76ea221a89687074e0c0dbd8fb80de2d9340e
kayzongwei <zongwei.kay.2014@smu.edu.sg>
2016-08-04 23:18:11 +0800

    Subuser Table View Cell

---
+++ FYP/Xcode/SmartHawker/SmartHawker/SubuserTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SubuserTableViewCell.swift
+//  SmartHawker
+//
+//  Created by Kay Zong Wei on 4/8/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+
+class SubuserTableViewCell: UITableViewCell {
+    
+    // Properties
+    // Labels
+    @IBOutlet weak var businessNameLabel: UILabel!
+    @IBOutlet weak var subuserNameLabel: UILabel!
+    
+    // Image View
+    @IBOutlet weak var userProfilePicImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
