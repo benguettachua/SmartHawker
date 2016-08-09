937697fc468d270ef16ed5635d3f864708cce33f
benguettachua <benguettachua@gmail.com>
2016-08-09 18:04:10 +0800

    done dao

--- FYP/Xcode/SmartHawker/SmartHawker/controllerDAO.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/controllerDAO.swift
@@ -9,62 +9,33 @@
 import UIKit
 
 class connectionDAO{
-    var tempCounter = 0
-    var toShare = ShareData.sharedInstance // This is to share the date selected to RecordViewController.
     
-    
-    func loadRecordsFromLocaDatastore(user: PFUser){
-        // Load from local datastore into UI.
-        toShare.records.removeAll()
+    //load records into controller
+    func loadRecords() -> [PFObject]{
         let query = PFQuery(className: "Record")
-        query.whereKey("user", equalTo: user)
+        var records = [PFObject]()
         query.fromLocalDatastore()
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [PFObject]?, error: NSError?) -> Void in
+        query.whereKey("user", equalTo: PFUser.currentUser()!)
+        do{
+            records = try query.findObjects()
+        } catch {
             
-            if error == nil {
-                // Do something with the found objects
-                if let objects = objects {
-                    
-                    for object in objects {
-                        let date = object["date"] as! String
-                        let type = object["type"] as! Int
-                        let amount = object["amount"] as! Double
-                        var localIdentifierString = object["subUser"]
-                        var recordedBy = object["subuser"]
-                        if (recordedBy == nil) {
-                            recordedBy = ""
-                        }
-                        var typeString = ""
-                        if (type == 0) {
-                            typeString = "Sales"
-                        } else if (type == 1) {
-                            typeString = "COGS"
-                        } else if (type == 2) {
-                            typeString = "Expenses"
-                        } else if (type == 3){
-                            typeString = "Expenses"
-                        }
-                        
-                        var description = object["description"]
-                        
-                        if (description == nil || description as! String == "") {
-                            description = "No description"
-                        }
-                        
-                        if (localIdentifierString == nil) {
-                            localIdentifierString = String(self.tempCounter += 1)
-                        }
-                        
-                        let newRecord = RecordTable(date: date, type: typeString, amount: amount, localIdentifier: localIdentifierString! as! String, description: description as! String, recordedUser: recordedBy as! String)
-                        self.toShare.records.append(newRecord)
-                        print(self.toShare.records.count)
-                    }
-                }
-            } else {
-                // Log details of the failure
-                print("Error: \(error!) \(error!.userInfo)")
-            }
         }
+        return records
+    }
+    
+    //load records into local datastore
+    func loadRecordsIntoLocalDatastore() -> [PFObject]{
+        // Part 1: Load from DB and pin into local datastore.
+        let query = PFQuery(className: "Record")
+        var records = [PFObject]()
+        query.whereKey("user", equalTo: PFUser.currentUser()!)
+        do{
+            records = try query.findObjects()
+            
+        } catch {
+            
+        }
+        return records
     }
 }
