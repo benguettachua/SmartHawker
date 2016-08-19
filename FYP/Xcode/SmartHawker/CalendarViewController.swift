300b837a3203245012608fd8c9c5a6ed92227066
benguettachua <benguettachua@gmail.com>
2016-08-19 13:23:21 +0800

    pushing calendar view

--- FYP/Xcode/SmartHawker/CalendarViewController.swift
+++ FYP/Xcode/SmartHawker/CalendarViewController.swift
@@ -28,7 +28,6 @@ class CalendarViewcontroller: UIViewController{
     var chosenMonthDate = NSDate()
     var calendarForChangeMonth = NSCalendar.init(calendarIdentifier: NSCalendarIdentifierGregorian)
     var tempCounter = 0
-    var records = [RecordTable]()
     
     typealias CompletionHandler = (success:Bool) -> Void
     let user = PFUser.currentUser()
@@ -41,30 +40,7 @@ class CalendarViewcontroller: UIViewController{
         didSet {
             // title = date.format("MMMM d, yyyy")
         }
-    }//for calendar
-    /*
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Formatting to format as saved in DB.
-        var correctDateString = ""
-        if toShare.storeDate == nil{
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "dd/MM/yyyy"
-            correctDateString = dateFormatter.stringFromDate(NSDate())
-        }else{
-            correctDateString = toShare.dateString
-        }
-        loadRecordsFromLocaDatastore({ (success) -> Void in
-            
-            self.loadRecords(correctDateString)
-            
-            
-        })
-        
-        
     }
-    */
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -77,18 +53,13 @@ class CalendarViewcontroller: UIViewController{
         }else{
             correctDateString = toShare.dateString
         }
-        loadRecordsFromLocaDatastore({ (success) -> Void in
-            
-            self.loadRecords(correctDateString)
-            
-            
-        })
         
         let isSubuser = toShare.isSubUser
         if (isSubuser) {
             self.logoutButton.hidden = false
         }
         
+        loadRecords(correctDateString)
         
         calendar.delegate = self
         calendar.backgroundColor = UIColor.clearColor()
@@ -135,7 +106,7 @@ class CalendarViewcontroller: UIViewController{
     @IBAction func logout(sender: UIButton) {
         let alert = UIAlertController(title: "Are you sure?", message: "Once logged out, you will have to log in again.", preferredStyle: .Alert)
         alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { Void in
-            PFUser.logOut()
+            connectionDAO().logout()
             self.view.window!.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: nil))
@@ -145,24 +116,12 @@ class CalendarViewcontroller: UIViewController{
     func loadRecords(correctDateString: String){
         var salesAmount = 0.0
         var expensesAmount = 0.0
-        var dates = [String]()
-        for record in self.records {
-            dates.append(record.date)
-            if record.date.containsString(correctDateString){
-                let type = record.type
-                let amount = Double(record.amount)
-                if (type == "Sales") {
-                    salesAmount += amount
-                } else if (type == "COGS") {
-                    expensesAmount += amount
-                } else if (type == "Expenses") {
-                    expensesAmount += amount
-                }
-            }
-            
-        }
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject(dates, forKey: "SavedDateArray")
+        
+        let values = CalendarController().values(correctDateString)
+
+        salesAmount = values.0
+        expensesAmount = values.1
+        
         // Sales Label
         let salesString2dp = "$" + String(format:"%.2f", salesAmount)
         self.salesText.text = salesString2dp
@@ -179,69 +138,6 @@ class CalendarViewcontroller: UIViewController{
         self.profitText.font = UIFont(name: profitText.font.fontName, size: 24)
     }
     
-        func loggedOut() {
-            PFUser.logOut()
-            self.performSegueWithIdentifier("logout", sender: self)
-    }
-    
-    func loadRecordsFromLocaDatastore(completionHandler: CompletionHandler) {
-        // Load from local datastore into UI.
-        self.records.removeAll()
-        let query = PFQuery(className: "Record")
-        let isSubUser = toShare.isSubUser
-        if (isSubUser) {
-            query.whereKey("subuser", equalTo: toShare.subuser)
-        }
-        query.whereKey("user", equalTo: user!)
-        query.fromLocalDatastore()
-        query.findObjectsInBackgroundWithBlock {
-            (objects: [PFObject]?, error: NSError?) -> Void in
-            
-            if error == nil {
-                // Do something with the found objects
-                if let objects = objects {
-                    for object in objects {
-                        let date = object["date"] as! String
-                        let type = object["type"] as! Int
-                        let amount = object["amount"] as! Double
-                        var localIdentifierString = object["subUser"]
-                        var recordedBy = object["subuser"]
-                        if (recordedBy == nil) {
-                            recordedBy = ""
-                        }
-                        var typeString = ""
-                        if (type == 0) {
-                            typeString = "Sales"
-                        } else if (type == 1) {
-                            typeString = "COGS"
-                        } else if (type == 2) {
-                            typeString = "Expenses"
-                        } else if (type == 3){
-                            typeString = "fixMonthlyExpenses"
-                        }
-                        
-                        var description = object["description"]
-                        
-                        if (description == nil || description as! String == "") {
-                            description = "No description"
-                        }
-                        
-                        if (localIdentifierString == nil) {
-                            localIdentifierString = String(self.tempCounter += 1)
-                        }
-                        
-                        let newRecord = RecordTable(date: date, type: typeString, amount: amount, localIdentifier: localIdentifierString! as! String, description: description as! String, recordedUser: recordedBy as! String)
-                        self.records.append(newRecord)
-                    }
-                    completionHandler(success: true)
-                }
-            } else {
-                // Log details of the failure
-                print("Error: \(error!) \(error!.userInfo)")
-                completionHandler(success: false)
-            }
-        }
-    }
     
     @IBAction func Record(sender: UIBarButtonItem) {
         
--- FYP/Xcode/SmartHawker/SmartHawker/CalendarController.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/CalendarController.swift
@@ -10,9 +10,44 @@ import Foundation
 
 class CalendarController{
     
+    var records = [PFObject]()
     
+    func loadRecords(){
+       
+        records = connectionDAO().loadRecords()
+        
+    }
     
-    
-    
+    func values(correctDateString: String) -> (Double, Double){
+        
+        records = connectionDAO().loadRecords()
+        
+        var salesAmount = 0.0
+        var expensesAmount = 0.0
+        var dates = [String]()
+        for record in self.records {
+            
+            let dateString = record["date"] as! String
+            let type = record["type"] as! Int
+            let amount = record["amount"] as! Double
+            
+            dates.append(record["date"] as! String)
+            if dateString.containsString(correctDateString){
+                
+                if (type == 0) {
+                    salesAmount += amount
+                } else if (type == 1) {
+                    expensesAmount += amount
+                } else if (type == 2) {
+                    expensesAmount += amount
+                }
+            }
+            
+        }
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(dates, forKey: "SavedDateArray")
+        
+        return (salesAmount, expensesAmount)
+    }
     
 }
