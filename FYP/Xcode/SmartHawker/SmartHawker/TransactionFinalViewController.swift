1ca24a4a68f4f0048fde81038f084fcdbaf6f492
gaominmell <gaominmell@gmail.com>
2016-08-24 23:25:35 +0800

    UI
    Edited transaction UI (50% Done)

---
+++ FYP/Xcode/SmartHawker/SmartHawker/TransactionFinalViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionFinalViewController.swift
+//  SmartHawker
+//
+//  Created by Gao Min on 24/08/2016.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+
+import UIKit
+import FontAwesome_iOS
+
+class TransactionFinalViewController: UIViewController {
+    
+    
+}
---
+++ FYP/Xcode/SmartHawker/SmartHawker/TransactionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TransactionViewController.swift
+//  SmartHawker
+//
+//  Created by Gao Min on 24/08/2016.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+import FontAwesome_iOS
+
+class TransactionViewController: UIViewController {
+    
+    
+}
