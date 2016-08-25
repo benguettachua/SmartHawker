e0dfa1d2fff13ae59fa143272275650acbb386c5
wendyggx <wendyggx@hotmail.com>
2016-08-25 22:12:59 +0800

    UI TRANSACTION PAGE
    TRANSACTION PAGE DONE!

--- FYP/Xcode/SmartHawker/SmartHawker/TransactionFinalViewController.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/TransactionFinalViewController.swift
@@ -13,4 +13,55 @@ import FontAwesome_iOS
 class TransactionFinalViewController: UIViewController {
     
     
+    @IBOutlet weak var backbtn: UIButton!
+    
+    @IBOutlet weak var donebtn: UIButton!
+    
+    @IBOutlet weak var todaybtn: UIButton!
+    
+    
+    @IBOutlet weak var descicon: UILabel!
+    
+    
+    @IBOutlet weak var imageicon: UILabel!
+    
+    @IBOutlet weak var addbtn: UIButton!
+    
+    override func viewDidLoad() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "handleTap:"))
+        
+        var faicon = [String: UniChar]()
+        faicon["faleftback"] = 0xf053
+        faicon["fatick"] = 0xf00c
+        faicon["facalendar"] = 0xf274
+        faicon["fadesc"] = 0xf044
+        faicon["faimage"] = 0xf03e
+
+        backbtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        backbtn.setTitle(String(format: "%C", faicon["faleftback"]!), forState: .Normal)
+        
+        donebtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        donebtn.setTitle(String(format: "%C", faicon["fatick"]!), forState: .Normal)
+        
+        todaybtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        todaybtn.setTitle(String(format: "%C", faicon["facalendar"]!), forState: .Normal)
+        
+        descicon.font = UIFont(name: "FontAwesome", size: 20)
+        
+        descicon.text = String(format: "%C", faicon["fadesc"]!)
+        
+        imageicon.font = UIFont(name: "FontAwesome", size: 20)
+        
+        imageicon.text = String(format: "%C", faicon["faimage"]!)
+        
+    }
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            view.endEditing(true)
+        }
+        sender.cancelsTouchesInView = false
+    }
 }
