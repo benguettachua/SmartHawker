0deae871c3b93c353c0bc1e292180959d52f081f
wendyggx <wendyggx@hotmail.com>
2016-08-26 17:52:29 +0800

    UI PROFILE PAGE
    Edited profile page and done!!!

--- FYP/Xcode/SmartHawker/SmartHawker/ContactUsPageView.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/ContactUsPageView.swift
@@ -13,8 +13,15 @@ class ContactUsPage: UIViewController {
     //MARK properties
     
     
+    @IBOutlet weak var backbtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        var faicon = [String: UniChar]()
+        faicon["faleftback"] = 0xf053
+        
+        backbtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        backbtn.setTitle(String(format: "%C", faicon["faleftback"]!), forState: .Normal)
     }
     
     @IBAction func back(sender: UIBarButtonItem){
