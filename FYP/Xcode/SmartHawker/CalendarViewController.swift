532e1993973bfb55ba0a795d1af931a2f31d4cd8
benguettachua <benguettachua@gmail.com>
2016-08-27 17:58:28 +0800

    added in JTAPPLECALENDAR

--- FYP/Xcode/SmartHawker/CalendarViewController.swift
+++ FYP/Xcode/SmartHawker/CalendarViewController.swift
@@ -7,21 +7,24 @@
 //
 
 import UIKit
-import CalendarView
+import JTAppleCalendar
 import SwiftMoment
 
 class CalendarViewcontroller: UIViewController{
     
     // Mark: Properties
     // Top Bar
-    @IBOutlet weak var todayRecordView: UIView!
-    @IBOutlet weak var logoutButton: UIButton!
 
-    @IBOutlet weak var calendar: CalendarView!
+    var numberOfRows = 6
+    
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    @IBOutlet weak var monthLabel: UILabel!
+    let formatter = NSDateFormatter()
+    let testCalendar: NSCalendar! = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+    
     @IBOutlet weak var profitText: UILabel!
     @IBOutlet weak var expensesText: UILabel!
     @IBOutlet weak var salesText: UILabel!
-    @IBOutlet weak var navBarLogoutButton: UIBarButtonItem!
     @IBOutlet weak var navBar: UINavigationItem!
     var day: String!
     var actualMonthDate = moment(NSDate())
@@ -43,7 +46,51 @@ class CalendarViewcontroller: UIViewController{
     }
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        formatter.dateFormat = "yyyy MM dd"
+        testCalendar.timeZone = NSTimeZone(abbreviation: "GMT")!
         
+        // Setting up your dataSource and delegate is manditory
+        //_____________________________________________________________________________________________
+        calendarView.delegate = self
+        calendarView.dataSource = self
+        //_____________________________________________________________________________________________
+        
+        
+        
+        // Registering your cells is manditory
+        //_____________________________________________________________________________________________
+        calendarView.registerCellViewXib(fileName: "CellView")
+        // You also can register by class
+        // calendarView.registerCellViewClass(fileName: "JTAppleCalendar_Example.CodeCellView")
+        //_____________________________________________________________________________________________
+        
+        
+        
+        // Enable/disable the following code line to show/hide headers.
+        
+        
+        // The following default code can be removed since they are already the default.
+        // They are only included here so that you can know what properties can be configured
+        //_____________________________________________________________________________________________
+        calendarView.direction = .Vertical                                 // default is horizontal
+        calendarView.cellInset = CGPoint(x: 0, y: 0)                         // default is (3,3)
+        calendarView.allowsMultipleSelection = false                         // default is false
+        calendarView.firstDayOfWeek = .Sunday                                // default is Sunday
+        calendarView.scrollEnabled = true                                    // default is true
+        calendarView.scrollingMode = .StopAtEachCalendarFrameWidth           // default is .StopAtEachCalendarFrameWidth
+        calendarView.itemSize = nil                                          // default is nil. Use a value here to change the size of your cells
+        calendarView.rangeSelectionWillBeUsed = false                        // default is false
+        //_____________________________________________________________________________________________
+        
+        // Reloading the data on viewDidLoad() is only necessary if you made LAYOUT changes eg. number of row per month change
+        // or changing the start day of week from sunday etc etc.
+        calendarView.reloadData()
+        
+        // After reloading. Scroll to your selected date, and setup your calendar
+        calendarView.scrollToDate(NSDate(), triggerScrollToDateDelegate: false, animateScroll: false) {
+            let currentDate = self.calendarView.currentCalendarDateSegment()
+            self.setupViewsOfCalendar(currentDate.dateRange.start, endDate: currentDate.dateRange.end)
+        }
         // Formatting to format as saved in DB.
         var correctDateString = ""
         if toShare.dateString == nil{
@@ -54,63 +101,11 @@ class CalendarViewcontroller: UIViewController{
             correctDateString = toShare.dateString
         }
         
-        let isSubuser = toShare.isSubUser
-        if (isSubuser) {
-            self.logoutButton.hidden = false
-        }
+
         
         loadRecords(correctDateString)
-        
-        calendar.delegate = self
-        calendar.backgroundColor = UIColor.clearColor()
-        if toShare.dateString != nil {
-            calendar.selectDate(toShare.storeDate)
-            self.MonthAndYear.text = self.toShare.storeDate.monthName.localized() + " / " + String(self.toShare.storeDate.year)
-            loadRecords(toShare.dateString)
-        }else{
-            calendar.selectDate(moment(NSDate()))
-        }
-        todayRecordView.backgroundColor = UIColor(white: 1, alpha: 0.3)
 
-    }
-    
-    @IBAction func nextMonth(sender: UIBarButtonItem) {
-        
-        let periodComponents = NSDateComponents()
-        periodComponents.month = +1
-        let newDate = calendarForChangeMonth!.dateByAddingComponents(
-            periodComponents,
-            toDate: chosenMonthDate,
-            options: [])!
-        actualMonthDate = moment(newDate)
-        chosenMonthDate = newDate
-        
-        calendar.selectDate(actualMonthDate)
-        MonthAndYear.text = actualMonthDate.monthName + " / " + String(actualMonthDate.year)
-    }
-    
-    @IBAction func previousMonth(sender: UIBarButtonItem) {
-        
-        let periodComponents = NSDateComponents()
-        periodComponents.month = -1
-        let newDate = calendarForChangeMonth!.dateByAddingComponents(
-            periodComponents,
-            toDate: chosenMonthDate,
-            options: [])!
-        actualMonthDate = moment(newDate)
-        chosenMonthDate = newDate
-        
-        calendar.selectDate(actualMonthDate)
-        MonthAndYear.text = actualMonthDate.monthName + " / " + String(actualMonthDate.year)
-    }
-    @IBAction func logout(sender: UIButton) {
-        let alert = UIAlertController(title: "Are you sure?", message: "Once logged out, you will have to log in again.", preferredStyle: .Alert)
-        alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { Void in
-            connectionDAO().logout()
-            self.view.window!.rootViewController?.dismissViewControllerAnimated(true, completion: nil)
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .Default, handler: nil))
-        self.presentViewController(alert, animated: true, completion: nil)
+
     }
     
     func loadRecords(correctDateString: String){
@@ -176,65 +171,90 @@ class CalendarViewcontroller: UIViewController{
         
     }
     
+    @IBAction func printSelectedDates() {
+        print("Selected dates --->")
+        for date in calendarView.selectedDates {
+            print(formatter.stringFromDate(date))
+        }
+    }
+    
+    
+    @IBAction func changeToOneRows() {
+        numberOfRows = 1
+        calendarView.reloadData()
+    }
+    
+    @IBAction func next(sender: UIButton) {
+        self.calendarView.scrollToNextSegment() {
+            let currentSegmentDates = self.calendarView.currentCalendarDateSegment()
+            self.setupViewsOfCalendar(currentSegmentDates.dateRange.start, endDate: currentSegmentDates.dateRange.end)
+        }
+    }
+    
+    @IBAction func previous(sender: UIButton) {
+        self.calendarView.scrollToPreviousSegment() {
+            let currentSegmentDates = self.calendarView.currentCalendarDateSegment()
+            self.setupViewsOfCalendar(currentSegmentDates.dateRange.start, endDate: currentSegmentDates.dateRange.end)
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func setupViewsOfCalendar(startDate: NSDate, endDate: NSDate) {
+        let month = testCalendar.component(NSCalendarUnit.Month, fromDate: startDate)
+        let monthName = NSDateFormatter().monthSymbols[(month) % 12] // 0 indexed array
+        let year = NSCalendar.currentCalendar().component(NSCalendarUnit.Year, fromDate: startDate)
+        monthLabel.text = monthName + " " + String(year)
+    }
+    
 }
 
-
-extension CalendarViewcontroller: CalendarViewDelegate {
-    
-    func calendarDidSelectDate(date: Moment) {
-        self.date = date
+// MARK : JTAppleCalendarDelegate
+extension CalendarViewcontroller: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    func configureCalendar(calendar: JTAppleCalendarView) -> (startDate: NSDate, endDate: NSDate, numberOfRows: Int, calendar: NSCalendar) {
         
-        // Adding 8 hours due to timezone
-        let duration = 8.hours
-        let dateInNSDate = date.add(duration).date
-
+        let todayDateString = formatter.stringFromDate(NSDate())
+        let thisYearString = todayDateString[todayDateString.startIndex...todayDateString.startIndex.advancedBy(3)]
+        let nextYear = Int(thisYearString)! + 1
+        let secondDate = formatter.dateFromString(String(nextYear) + " 12 31")
         
-        // Formatting to format as saved in DB.
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "dd/MM/yyyy"
-        let correctDateString = dateFormatter.stringFromDate(dateInNSDate)
-        
-        //for displaying date
-        if date.day == 1 || date.day == 21{
-            self.day = String(date.day) + "st".localized()
-            
-        }else if date.day == 2 || date.day == 22{
-            self.day = String(date.day) + "nd".localized()
-            
-        }else if date.day == 3 || date.day == 23{
-            self.day = String(date.day) + "rd".localized()
-            
-        }else{
-            self.day = String(date.day) + "th".localized()
-        }
-
-        var toDisplayDate = ""
-        if lang == "zh-Hans" {
-            toDisplayDate = date.monthName.localized() + " \(self.day) " + " \(date.year) 年, "+(date.weekdayName).localized()
-        }else{
-            toDisplayDate = self.day + " " + date.monthName + " " + String(date.year) + " , " + date.weekdayName
-        }
-        toShare.storeDate = date
-        toShare.dateString = correctDateString
-        toShare.toDisplayDate = toDisplayDate
-        loadRecords(correctDateString)
+        let firstDate = formatter.dateFromString("2016 01 01")
+        let aCalendar = NSCalendar.currentCalendar() // Properly configure your calendar to your time zone here
+        return (startDate: firstDate!, endDate: secondDate!, numberOfRows: numberOfRows, calendar: aCalendar)
     }
     
-    func calendarDidPageToDate(date: Moment) {
-
-        self.date = date
-        if toShare.storeDate == nil {
-            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                self.MonthAndYear.text = date.monthName.localized() + " / " + String    (date.year)
-            })
-        } else {
-            self.MonthAndYear.text = self.toShare.storeDate.monthName.localized() + " / " + String(self.toShare.storeDate.year)
-
-
-        }
-
+    func calendar(calendar: JTAppleCalendarView, isAboutToDisplayCell cell: JTAppleDayCellView, date: NSDate, cellState: CellState) {
+        (cell as? CellView)?.setupCellBeforeDisplay(cellState, date: date)
     }
     
+    func calendar(calendar: JTAppleCalendarView, didDeselectDate date: NSDate, cell: JTAppleDayCellView?, cellState: CellState) {
+        (cell as? CellView)?.cellSelectionChanged(cellState)
+    }
     
+    func calendar(calendar: JTAppleCalendarView, didSelectDate date: NSDate, cell: JTAppleDayCellView?, cellState: CellState) {
+        (cell as? CellView)?.cellSelectionChanged(cellState)
+        
+        print("lala")
+        printSelectedDates()
+    }
     
+    func calendar(calendar: JTAppleCalendarView, isAboutToResetCell cell: JTAppleDayCellView) {
+        (cell as? CellView)?.selectedView.hidden = true
+    }
+    
+    func calendar(calendar: JTAppleCalendarView, didScrollToDateSegmentStartingWithdate startDate: NSDate, endingWithDate endDate: NSDate) {
+        setupViewsOfCalendar(startDate, endDate: endDate)
+    }
+    
+}
+
+func delayRunOnMainThread(delay:Double, closure:()->()) {
+    dispatch_after(
+        dispatch_time(
+            DISPATCH_TIME_NOW,
+            Int64(delay * Double(NSEC_PER_SEC))
+        ),
+        dispatch_get_main_queue(), closure)
 }
