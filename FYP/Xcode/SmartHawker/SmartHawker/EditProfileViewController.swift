56ffaf6819dbd8f2fa536415fa4806aed3eeb865
wendyggx <wendyggx@hotmail.com>
2016-08-29 16:38:42 +0800

    UI
    Added “change profile picture” button and label

--- FYP/Xcode/SmartHawker/SmartHawker/EditProfileViewController.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/EditProfileViewController.swift
@@ -43,38 +43,38 @@ class EditProfileViewController: UIViewController {
         name.placeholder = "NAME"
         name.text = "ang boon chang"
         
-        view.layout(name).top(170).horizontally(left: 20, right: 20).height(22)
+        view.layout(name).top(200).horizontally(left: 20, right: 20).height(22)
         
         let phone = TextField()
         phone.placeholder = "PHONE"
         phone.text = "98765432"
         
-        view.layout(phone).top(215).horizontally(left: 20, right: 20).height(22)
+        view.layout(phone).top(245).horizontally(left: 20, right: 20).height(22)
         
         let email = TextField()
         email.placeholder = "EMAIL"
         email.text = "abc@sis"
 
-        view.layout(email).top(260).horizontally(left: 20, right: 20).height(22)
+        view.layout(email).top(290).horizontally(left: 20, right: 20).height(22)
         
         let bizname = TextField()
         bizname.placeholder = "BUSINESS NAME"
         bizname.text = "abc pte ltd"
 
     
-    view.layout(bizname).top(305).horizontally(left: 20, right: 20).height(22)
+    view.layout(bizname).top(335).horizontally(left: 20, right: 20).height(22)
         
         let biznum = TextField()
         biznum.placeholder = "BUSINESS NUMBER"
         biznum.text = "12345678"
 
-        view.layout(biznum).top(350).horizontally(left: 20, right: 20).height(22)
+        view.layout(biznum).top(380).horizontally(left: 20, right: 20).height(22)
         
         let address = TextField()
         address.placeholder = "ADDRESS"
         address.text = "21 seletar road"
 
-    view.layout(address).top(395).horizontally(left: 20, right: 20).height(22)
+    view.layout(address).top(425).horizontally(left: 20, right: 20).height(22)
     }
     
     func handleTap(sender: UITapGestureRecognizer) {
