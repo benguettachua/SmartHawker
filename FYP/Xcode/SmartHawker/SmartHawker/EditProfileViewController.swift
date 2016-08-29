43c3f98302cbddd383f9b1963ec5b2255f196a97
wendyggx <wendyggx@hotmail.com>
2016-08-29 15:50:27 +0800

    UI
    Added edit profile

---
+++ FYP/Xcode/SmartHawker/SmartHawker/EditProfileViewController.swift
@@ -0,0 +1,87 @@
+//
+//  EditProfileViewController.swift
+//  SmartHawker
+//
+//  Created by GX on 29/8/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import Material
+import FontAwesome_iOS
+import UIKit
+
+
+class EditProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var backbtn: UIButton!
+    @IBOutlet weak var donebtn: UIButton!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    
+        
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "handleTap:"))
+
+        var faicon = [String: UniChar]()
+        faicon["faleftback"] = 0xf053
+        faicon["fatick"] = 0xf00c
+
+        backbtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        backbtn.setTitle(String(format: "%C", faicon["faleftback"]!), forState: .Normal)
+        
+        donebtn.titleLabel!.font = UIFont(name: "FontAwesome", size: 20)
+        
+        donebtn.setTitle(String(format: "%C", faicon["fatick"]!), forState: .Normal)
+        
+        let name = TextField()
+        name.placeholder = "NAME"
+        name.text = "ang boon chang"
+        
+        view.layout(name).top(170).horizontally(left: 20, right: 20).height(22)
+        
+        let phone = TextField()
+        phone.placeholder = "PHONE"
+        phone.text = "98765432"
+        
+        view.layout(phone).top(215).horizontally(left: 20, right: 20).height(22)
+        
+        let email = TextField()
+        email.placeholder = "EMAIL"
+        email.text = "abc@sis"
+
+        view.layout(email).top(260).horizontally(left: 20, right: 20).height(22)
+        
+        let bizname = TextField()
+        bizname.placeholder = "BUSINESS NAME"
+        bizname.text = "abc pte ltd"
+
+    
+    view.layout(bizname).top(305).horizontally(left: 20, right: 20).height(22)
+        
+        let biznum = TextField()
+        biznum.placeholder = "BUSINESS NUMBER"
+        biznum.text = "12345678"
+
+        view.layout(biznum).top(350).horizontally(left: 20, right: 20).height(22)
+        
+        let address = TextField()
+        address.placeholder = "ADDRESS"
+        address.text = "21 seletar road"
+
+    view.layout(address).top(395).horizontally(left: 20, right: 20).height(22)
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            view.endEditing(true)
+        }
+        sender.cancelsTouchesInView = false
+    }
+    
+}
