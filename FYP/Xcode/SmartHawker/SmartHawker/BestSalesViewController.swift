b70de2a65ed8f1e9660bf8412c531636487a13e9
Kay Zong Wei <zongwei.kay.2014@smu.edu.sg>
2016-09-10 06:10:49 +0800

    xiao link best sales page

---
+++ FYP/Xcode/SmartHawker/SmartHawker/BestSalesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BestSalesViewController.swift
+//  SmartHawker
+//
+//  Created by Kay Zong Wei on 10/9/16.
+//  Copyright © 2016 Kay Zong Wei. All rights reserved.
+//
+
+import UIKit
+
+class BestSalesViewController: UIViewController {
+    
+    // MARK: Properties
+    @IBOutlet weak var categorySegmentControl: UISegmentedControl!
+    
+    
+    
+}
