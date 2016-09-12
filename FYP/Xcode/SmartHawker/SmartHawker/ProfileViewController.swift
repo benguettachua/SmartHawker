7007b21ebb1767bde0a12e359b2a281b821c9149
Kay Zong Wei <zongwei.kay.2014@smu.edu.sg>
2016-09-12 16:42:09 +0800

    DELETED SUBUSER

--- FYP/Xcode/SmartHawker/SmartHawker/ProfileViewController.swift
+++ FYP/Xcode/SmartHawker/SmartHawker/ProfileViewController.swift
@@ -182,15 +182,7 @@ class ProfileViewController: UITableViewController {
             if (selectedRow == 1) {
                 self.performSegueWithIdentifier("toSettings", sender: self)
             }
-            
-            // Subuser
-            if (selectedRow == 2) {
-                let comingSoonAlert = UIAlertController(title: "Coming soon".localized(), message: "Function currently developing!".localized(), preferredStyle: .Alert)
-                comingSoonAlert.addAction(UIAlertAction(title: "Ok".localized(), style: .Default, handler: nil))
-                self.presentViewController(comingSoonAlert, animated: true, completion: nil)
-            }
         }
     }
-
 
 }
